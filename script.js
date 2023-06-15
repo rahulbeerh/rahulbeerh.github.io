a37881117100dd642c36d0beb7a16db86db7b2b0
rahulbeerh <129044167+rahulbeerh@users.noreply.github.com>
2023-06-15 13:44:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(".menu-bar").on("click",function(){
+    $(".drop-down-menu").toggleClass("visible");
+})
+
+$(".drop-down-menu-links").on("click",function(){
+    $(".drop-down-menu").toggleClass("visible");
+})
